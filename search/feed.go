e779d3501aafead375275a384084b6f7f05fb8f0
Richard Palmer <richard.m.palmer@gmail.com>
2025-02-11 13:06:10 -0600

    Adding more implementation

---
+++ search/feed.go
@@ -0,0 +1,31 @@
+package search
+
+import (
+	"encoding/json"
+	"os"
+)
+const dataFile = "data/data.json"
+
+tpe Feed struct {
+	Name string `json:"site"`
+	URI  string `json:"link"`
+	Type string `json:"type"`
+}
+
+func RetrieveFeeds() ([]*Feed, error) {
+	//open the file
+	file,err := os.Open(dataFile)
+	if err := nil {
+		return nil,err
+	}
+
+	// schedule the file to be closed
+	defer file.Close()
+
+	// decode the file into a slice of pointers
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
+
